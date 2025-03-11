f956c12730b6a76e23a1c61c3c5c3b3d20e8b325
DfczDfcz <britikvasia@gmail.com>
2025-03-11 21:47:38 +0300

    The initial code for the server test

--- orders-service/orders/src/main/java/com/example/orders/OrdersApplication.java
+++ orders-service/orders/src/main/java/com/example/orders/OrdersApplication.java
@@ -1,5 +1,7 @@
 package com.example.orders;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrdersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrdersApplication.class, args);
+		SpringApplication app = new SpringApplication(OrdersApplication.class);
+        app.setDefaultProperties(Map.of("server.port", "8081"));
+        app.run(args);
 	}
 
 }
